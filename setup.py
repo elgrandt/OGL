7b9d459e4aae7410575714db10f11b1402569992
elgrandt <dantasat11@hotmail.com>
2017-07-29 18:14:42 -0300

    Update setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="OGL",
+    version="0.1",
+    author="Dylan Tasat",
+    author_email="dylantasat11@hotmail.com",
+    url="https://github.com/elgrandt/OGL",
+    packages=find_packages(),
+)
